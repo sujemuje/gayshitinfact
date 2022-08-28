95884d2233e01cf871897b4239c603ff4874e985
Michał Kozłowski <contact.michal.kozlowski@gmail.com>
2022-08-28 19:16:58 +0200

    skille

--- skills.py
+++ skills.py
@@ -1,3 +1,5 @@
+import copy
+import settings
 import skill
 import particle
 import arcade
@@ -5,21 +7,34 @@ from pygame.math import Vector2 as Vec
 
 
 class FireballProjectile(particle.BaseParticle):
-    def __init__(self, pos: Vec, trajectory: Vec):
+    def __init__(self, player, pos: Vec, trajectory: Vec):
+        self.player = player
         self.pos = pos
-        self.speed = 1
-        self.sprite = arcade.sprite.Sprite('holy shit.png')
+        self.speed = 1000
+        self.sprite = arcade.sprite.Sprite('holy shit.png', scale=.2)
         self.trajectory = trajectory
+        self.rm = False
 
     def on_update(self, dt) -> None:
-        self.pos += self.trajectory * dt
+        self.pos += self.trajectory * dt * self.speed
+        if -500 > self.pos.x or self.pos.x > settings.WINDOW_WIDTH + 500:
+            self.rm = True
+        if -500 > self.pos.y or self.pos.y > settings.WINDOW_HEIGHT + 500:
+            self.rm = True
+        for i in range(len(self.player.game.enemies)):
+            if self.sprite.collides_with_sprite(self.player.game.enemies[i].sprite):
+                self.player.game.enemies[i] = None
+        try:
+            self.player.game.enemies.remove(None)
+        except:
+            pass  # XD
 
     def on_draw(self) -> None:
         self.sprite.set_position(self.pos.x, self.pos.y)
         self.sprite.draw()
 
     def removable(self) -> bool:
-        return True
+        return self.rm
 
 
 class SkillQ(skill.BaseSkill):
@@ -29,8 +44,11 @@ class SkillQ(skill.BaseSkill):
 
     def on_update(self, dt) -> None:
         super().on_update(dt)  # UI interface management in BaseSkill class
-        for projectile in self.projectiles:
+        for i in range(len(self.projectiles)):
+            projectile = self.projectiles[i]
             projectile.on_update(dt)
+            if projectile.removable():
+                self.projectiles.pop(i)
 
     def on_draw(self, i) -> None:
         super().on_draw(i)  # UI interface management in BaseSkill class
@@ -38,7 +56,10 @@ class SkillQ(skill.BaseSkill):
             projectile.on_draw()
 
     def cast(self, mouse_x, mouse_y) -> None:
-        v = Vec(mouse_x, mouse_y) - self.player.pos
-        if v == Vec(0, 0): v = Vec(1, 0)
-        else: v = v.normalize()
-        self.projectiles.append(FireballProjectile(self.player.pos, v))
+        player_pos = Vec(self.player.pos.x, self.player.pos.y)
+        v = Vec(mouse_x, mouse_y) - player_pos
+        if v == Vec(0, 0):
+            v = Vec(1, 0)
+        else:
+            v = v.normalize()
+        self.projectiles.append(FireballProjectile(self.player, player_pos, v))
